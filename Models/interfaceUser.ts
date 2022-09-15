5c1f6307e70bec1b8f92b6d363d502460701ff76
ChriAsc <s1084723@studenti.univpm.it>
2022-09-15 08:09:41 +0200

    minor fix

--- Models/interfaceUser.ts
+++ Models/interfaceUser.ts
@@ -2,6 +2,7 @@
  * Interfaccia di User utile ad imporre vincoli e ad avere maggiore controllo
  */
 export interface interfaceUser {
+    addUser(username: string, email: string, budget: number): Promise<any>;
     findByName(username: string): Promise<any>;
     findByEmail(email: string): Promise<any>;
     getBudget(username: string): Promise<number>;
